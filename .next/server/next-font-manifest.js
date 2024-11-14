7b309fd1dfbf627c92b4f977ca2fbc44144eab13
leoromero97 <leonardo.g.romero.dev@gmail.com>
2024-11-14 19:18:48 -0300

    clear .next folder

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/leoromero/Desktop/projects/organigrama-nxc/src/app/layout\":[\"static/media/7ebd901f2f4a0b98-s.p.woff2\",\"static/media/95a978e26cc29d74-s.p.woff2\",\"static/media/f5e5067cd50e2c82-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
